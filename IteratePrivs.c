34af74bd54bcc64b05c3666c5eb1f3926399720c
stryker2k2 <stryker2k2@msn.com>
2022-07-15 09:31:18 -0500

    Update IteratePrivs.c
    Fixed errors detected by different compiler

--- IteratePrivs.c
+++ IteratePrivs.c
@@ -2,7 +2,7 @@
  * Description: Basic Template for Iterating Process Privileges
  * Author: @stryker2k2
  * Link: github.com/stryker2k2/iteratePrivs
- * Version: 1.0
+ * Version: 1.0.1
  * Compile: gcc IteratePrivs.c -o IteratePrivs.exe
  * Execution: IteratePrivs.exe
  */
@@ -11,12 +11,12 @@
 #include <windows.h>
 #include <stdbool.h>
 
-LPSTR lpName;
-PLUID lpLuid;
-LPDWORD lpNameSize;
 HANDLE token;
 TOKEN_PRIVILEGES tokenPrivs;
 DWORD TokenPrivsLength;
+LUID myLUID;
+LPWSTR privName;
+DWORD size = MAXDWORD32;
 
 int main ()
 {
@@ -30,28 +30,24 @@ int main ()
     if(!GetTokenInformation(token, TokenPrivileges, &tokenPrivs, TokenPrivsLength, &TokenPrivsLength))
     {
       printf("[-] Failed to open current process token\n");
-      printf("[-] Error Code: %d\n", GetLastError());
-      printf("[-] Token Info Size: %d\n", TokenPrivsLength);
+      printf("[-] Error Code: %ld\n", GetLastError());
+      printf("[-] Token Info Size: %ld\n", TokenPrivsLength);
       CloseHandle(token);
     }
   }
 
-  printf("[+] Privilege Count: %d\n", tokenPrivs.PrivilegeCount);
+  printf("[+] Privilege Count: %ld\n", tokenPrivs.PrivilegeCount);
 
   if (tokenPrivs.PrivilegeCount > 0)
   {
     int i;
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < (int)tokenPrivs.PrivilegeCount; i++)
     {
-      LUID myLUID;
-      LPWSTR privName;
-      DWORD size = MAXDWORD32;
-      
       myLUID = tokenPrivs.Privileges[i].Luid;
 
       //printf("[+] myLUID is: 0x%04X%04X\n", myLUID.HighPart, myLUID.LowPart);
       LookupPrivilegeNameW(NULL, &myLUID, privName, &size);
-      printf("[+] Privilege Name: %ls (0x%04X%04X)\n", privName, myLUID.HighPart, myLUID.LowPart);
+      printf("[+] Privilege Name: %ls (0x%04lX%04lX)\n", privName, myLUID.HighPart, myLUID.LowPart);
     }
   }
 }
